89f8c19ef740d761318b42626ee0594ce79d34ca
Emil Soelberg <millerspost@hotmail.com>
2014-03-11 14:46:49 +0100

    opsætning af forside.

--- index.php
+++ index.php
@@ -17,130 +17,215 @@
 
 <body>
 	<div class="col-xs-12 col-sm-12 col-md-12 container">
-		<wrapper class="col-xs-12 col-sm-12 col-md-12">
-			<header class="col-xs-12 col-sm-12 col-md-12">
-				<div class="logo">
-					<img src="img/" alt="Cashbackcard logo" class="img-responsive">
-				</div>
-				<ul>
-					<li><a href="#">Se Videoen</a></li>
-					<li><a href="#">Opret</a></li>
-					<li><a href="#">Log ind</a></li>
-				</ul>
+		<div class="col-xs-12 col-sm-12 col-md-12 wrapper">
+			<div class="row">
+				<header class="col-xs-12 col-sm-12 col-md-12">
+					<div class="logo">
+						<img src="img/" alt="Cashbackcard logo" class="img-responsive">
+					</div>
+					<ul>
+						<li><a href="#">Se Videoen</a></li>
+						<li><a href="#">Opret</a></li>
+						<li><a href="#">Log ind</a></li>
+					</ul>
 
-			</header>
+				</header>
+			</div><?php/* /row */?>
 			<?php/* frontpic starter med baggrundsbillede */?>
-			<div class="col-xs-12 col-sm-12 col-md-12 frontpic">
-				<div class="col-xs-6 col-sm-6 col-md-6 textbox">
-					<div class="frontlogo">
-						<img src="img/" alt="" class="img-responsive">
-					</div>
-					<h2>Slip for klubkort, benyt dit dankort<br>
-						og saml penge, ikke point</h2>
-					<div class="chip">
-						<img src="img/" alt="" class="img-responsive">
-					</div>
-					<div class="signupknap">
-						<p>Registrer dig</p>
-					</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 frontpic">
+					<div class="col-xs-6 col-sm-6 col-md-6 textbox">
+						<div class="frontlogo">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+						<h2>
+							Slip for klubkort, benyt dit dankort<br>
+							og saml penge, ikke point
+						</h2>
+						<div class="chip">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+						<div class="signupknap">
+							<button class="signupknap">
+								Registrer dig
+							</button>
+						</div>
 
-				</div><?php/* /textbox */?>
-				<div class="clear"></div>
-			</div> <?php/* /frontpic */?>
+					</div><?php/* /textbox */?>
+					<div class="clear"></div>
+				</div> <?php/* /frontpic */?>
+			</div><?php/* /row */?>
 
-			<div class="trekant">
-				<?php/* billede ind her? */?>
+			<div class="row">
+				<?php/* trekant ligger over hvid kasse */?>
+				<div class="trekant">
+					<?php/* billede ind her? */?>
+					<img src="img/" alt="" class="img-responsive">
+				</div>
 				
+
+				<?php/* hvis kasse med steps starter */?>
+				<div class="col-xs-12 col-sm-12 col-md-12 hvidkasse">
+					<div class="col-xs-8 col-sm-8 col-md-8 step1">
+						<div class="col-md-5 step1text">
+							<p>
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla
+								bla bla bla bla bla bla bla bla bla bla bla
+							</p>
+						</div>
+
+						<div class="col-md-2 step1ikon">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+
+						<div class="col-md-5 step1pic">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+					</div><?php/* /step */?>
+
+					<div class="col-xs-8 col-sm-8 col-md-8 step2">
+
+						<div class="col-md-5 step2pic">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+
+						<div class="col-md-2 step2ikon">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+
+						<div class="col-md-5 step2text">
+							<p>
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla
+								bla bla bla bla bla bla bla bla bla bla bla
+								bla bla bla bla bla bla bla bla bla bla bla
+							</p>
+						</div>				
+					</div><?php/* /step */?>
+					<div class="col-xs-8 col-sm-8 col-md-8 step3">
+
+						<div class="col-md-5 step3text">
+							<p>
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								
+							</p>
+						</div>
+						<div class="col-md-2 step3ikon">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+
+						<div class="col-md-5 step3pic">
+							<img src="img/" alt="" class="img-responsive">
+						</div>			
+					</div><?php/* /step */?>
+					<div class="col-xs-8 col-sm-8 col-md-8 step4">
+
+						<div class="col-md-5 step4pic">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+
+						<div class="col-md-2 step4ikon">
+							<img src="img/" alt="" class="img-responsive">
+						</div>
+
+						<div class="col-md-5 step4text">
+							<p>
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla 
+								bla bla bla bla bla bla bla bla bla bla bla
+							</p>
+						</div>
+						
+					</div><?php/* /step */?>
+					<div class="clear"></div>
+				</div><?php/* /hvidkasse */?>
+
+				<div class="col-xs-12 col-sm-12 col-md-12 moenster">
+					<img src="img/" alt="" class="img-responsive">
+				</div><?php/* moenster*/?>
+			</div><?php/* /row */?>
+
+			<div class="row">
+				<div class="col-xs-8 col-sm-8 col-md-8 testimonials">
+					<div class="testimonialText">
+						<h2>dont just takeour word for it</h2>
+						<h3>here is what others are saying</h3>
+						<img src="img/" alt="" class="img-responsive">
+					</div>
+					<div class="testimonialSlider">
+						slider . ? 
+					</div>
+				</div><?php/* /testimonials */?>
+			</div><?php/* /row */?>
+
+			<div class="row">
+				<div class="trekanter">
+					<img src="img/" alt="" class="img-responsive">
+				</div>
+
+				<div class="videoKasse">
+					<div class="videoText">
+						<h2>Easiest rewards programme</h2>
+						<h2>--- on the planet ---</h2>
+					</div>
+					<div class="videoFrame">
+						<iframe src="www.youtube.com"></iframe>
+					</div>
+				</div>
+
+				<div class="trekanter">
+					<img src="img/" alt="" class="img-responsive">
+				</div>
+			</div><?php/* /row */?>
+
+			<div class="row">
+				<div class="onlinePartner">
+					<!-- background image -->
+					<h2>--- what are you ---</h2>
+					<h3>waiting for?</h3>
+					<h4>--   there is free  --</h4>
+					<h4>money waiting on every corner</h4>
+				</div>
+
 			</div>
 
-			<?php/* hvis kasse med steps starter */?>
-			<div class="col-xs-12 col-sm-12 col-md-12 hvidkasse">
-				<div class="col-xs-8 col-sm-8 col-md-8 step1">
-					<div class="step1text">
-						<p>
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-
-						</p>
-					</div>
-
-					<div class="step1ikon">
-						<img src="img/" alt="" class="img-responsive">
-					</div>
-
-					<div class="step1pic">
-						<img src="img/" alt="" class="img-responsive">
-					</div>
-
+			<div class="row">
+				<div class="hvidTrekant">
+					<img src="img/" alt="" class="img-responsive">
 				</div>
-				<div class="col-xs-8 col-sm-8 col-md-8 step2">
-					<div class="step2ikon">
-					</div>
+			</div><?php/* /row */?>
 
-					<div class="step2text">
-						<p>
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							
-						</p>
-					</div>
-				
-				</div>
-				<div class="col-xs-8 col-sm-8 col-md-8 step3">
-					<div class="step3ikon">
-						<img src="img/" alt="" class="img-responsive">
-					</div>
 
-					<div class="step3text">
-						<p>
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							
-						</p>
-					</div>
-				
-				</div>
-				<div class="col-xs-8 col-sm-8 col-md-8 step4">
-					<div class="step4ikon">
-						<img src="img/" alt="" class="img-responsive">
-					</div>
 
-					<div class="step4text">
-						<p>
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							bla bla bla bla bla bla bla bla bla bla bla 
-							
-						</p>
-					</div>
-					
-				</div><?php/* /step1 */?>
-				<div class="clear"></div>
-			</div><?php/* /hvidkasse */?>
 
 		<div class="clear"></div>
-		</wrapper><?php/* /wrapper */?>
+		</div><?php/* /wrapper */?>
 	<div class="clear"></div>
 	</div><?php/* /container */?>
 <div class="clear"></div>
