079051ee192ef775ecc65fa02f39d85de58839fb
Idaebbesen <idaebbesen91@hotmail.com>
2014-03-11 10:50:54 +0100

    index og styles

---
+++ CBC.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+		<link href="styles.css" rel="stylesheet" type="text/css" />
+<title>Cashbackcard</title>
+</head>
+
+<body>
+	<div class="col-xs-12 col-sm-12 col-md-12 container">
+		<div class="col-xs-12 col-sm-12 col-md-12 wrapper">
+			<div class="col-xs-12 col-sm-12 col-md-12 header">
+
+			</header>
+
+			<div class="col-xs-12 col-sm-12 col-md-12 frontpic">
+				<div class="col-xs-6 col-sm-6 col-md-6 textbox">
+					<div class="frontlogo">
+					</div>
+					<h2>Slip for klubkort, benyt dit dankort<br>
+						og saml penge, ikke point</h1>
+					<div class="chip" img src="">
+					</div>
+					<div class="signupknap">
+						<p>Registrer dig</p>
+					<div>
+
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-12 hvidkasse">
+				<div class="col-xs-8 col-sm-8 col-md-8 step1">
+					<div class="step1pic">
+					</div>
+
+						<div class="step1ikon">
+						</div>
+
+						<div class="step1text">
+						</div>
+
+					</div>
+					<div class="col-xs-8 col-sm-8 col-md-8 step2">
+						<div class="step2ikon">
+						</div>
+
+						<div class="step2tekst">
+						</div>
+					
+					</div>
+					<div class="col-xs-8 col-sm-8 col-md-8 step3">
+						<div class="step3ikon">
+						</div>
+
+						<div class="step3tekst">
+						</div>
+					
+					</div>
+					<div class="col-xs-8 col-sm-8 col-md-8 step4">
+						<div class="step4ikon">
+						</div>
+
+						<div class="step4tekst">
+						</div>
+
+					
+				</div>
+			</div>
+
+		<div class="clear"></div>
+		</wrapper>
+	<div class="clear"></div>
+	</container>
+<div class="clear"></div>
+</body>
+</html>
