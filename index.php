8d6edfe0cc3dd8c3276e0ed57e3a5239c3b88067
Emil Soelberg <millerspost@hotmail.com>
2014-03-11 12:05:12 +0100

    start up
    bootstrap tilføjet, virker det?
    opsætning og css testes.

---
+++ index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<?php/* bootstrap links */?>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css.map" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css.map" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+	<link href="css/reset.css" rel="stylesheet" type="text/css" />
+	<link href="styles.css" rel="stylesheet" type="text/css" />
+	<title>Cashbackcard</title>
+</head>
+
+<body>
+	<div class="col-xs-12 col-sm-12 col-md-12 container">
+		<wrapper class="col-xs-12 col-sm-12 col-md-12">
+			<header class="col-xs-12 col-sm-12 col-md-12">
+				<div class="logo">
+					<img src="img/" alt="Cashbackcard logo" class="img-responsive">
+				</div>
+				<ul>
+					<li><a href="#">Se Videoen</a></li>
+					<li><a href="#">Opret</a></li>
+					<li><a href="#">Log ind</a></li>
+				</ul>
+
+			</header>
+			<?php/* frontpic starter med baggrundsbillede */?>
+			<div class="col-xs-12 col-sm-12 col-md-12 frontpic">
+				<div class="col-xs-6 col-sm-6 col-md-6 textbox">
+					<div class="frontlogo">
+						<img src="img/" alt="" class="img-responsive">
+					</div>
+					<h2>Slip for klubkort, benyt dit dankort<br>
+						og saml penge, ikke point</h2>
+					<div class="chip">
+						<img src="img/" alt="" class="img-responsive">
+					</div>
+					<div class="signupknap">
+						<p>Registrer dig</p>
+					</div>
+
+				</div><?php/* /textbox */?>
+				<div class="clear"></div>
+			</div> <?php/* /frontpic */?>
+
+			<div class="trekant">
+				<?php/* billede ind her? */?>
+				
+			</div>
+
+			<?php/* hvis kasse med steps starter */?>
+			<div class="col-xs-12 col-sm-12 col-md-12 hvidkasse">
+				<div class="col-xs-8 col-sm-8 col-md-8 step1">
+					<div class="step1text">
+						<p>
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+
+						</p>
+					</div>
+
+					<div class="step1ikon">
+						<img src="img/" alt="" class="img-responsive">
+					</div>
+
+					<div class="step1pic">
+						<img src="img/" alt="" class="img-responsive">
+					</div>
+
+				</div>
+				<div class="col-xs-8 col-sm-8 col-md-8 step2">
+					<div class="step2ikon">
+					</div>
+
+					<div class="step2text">
+						<p>
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							
+						</p>
+					</div>
+				
+				</div>
+				<div class="col-xs-8 col-sm-8 col-md-8 step3">
+					<div class="step3ikon">
+						<img src="img/" alt="" class="img-responsive">
+					</div>
+
+					<div class="step3text">
+						<p>
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							
+						</p>
+					</div>
+				
+				</div>
+				<div class="col-xs-8 col-sm-8 col-md-8 step4">
+					<div class="step4ikon">
+						<img src="img/" alt="" class="img-responsive">
+					</div>
+
+					<div class="step4text">
+						<p>
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							bla bla bla bla bla bla bla bla bla bla bla 
+							
+						</p>
+					</div>
+					
+				</div><?php/* /step1 */?>
+				<div class="clear"></div>
+			</div><?php/* /hvidkasse */?>
+
+		<div class="clear"></div>
+		</wrapper><?php/* /wrapper */?>
+	<div class="clear"></div>
+	</div><?php/* /container */?>
+<div class="clear"></div>
+</body>
+</html>
